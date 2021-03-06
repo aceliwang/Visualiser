dce5aa0f4fec3082cb9f17719ed8c18464558594
aceliwang <acelidevice@gmail.com>
2021-03-06 04:11:00 +0000

    save

--- javascript.js
+++ javascript.js
@@ -10,38 +10,36 @@ let started = false
 
 // general flow: sound desk >> computer (web audio >> OBS >> resolume) >> arduino >> LED panel
 
-let macMode = function() {
-    navigator.mediaDevices.getUserMedia({audio: true})
-    .then(function(stream) {
-        initiateNode(stream)
-    })
+let macMode = function () {
+    navigator.mediaDevices.getUserMedia({ audio: true })
+        .then(function (stream) {
+            initiateNode(stream)
+        })
 }
 
 let initiateNode = function (stream) {
     let source = audioCtx.createMediaStreamSource(stream)
     source.connect(analyser)
     console.log(stream)
-    visualise() 
+    visualise()
+    started = true
 }
 
 // create media stream from mic input
-canvas.onclick = function() {
-    if (started) {
-        return
-    }
-    started = true
+canvas.onclick = function () {
+    if (started) { return }
     let audioCtx = new AudioContext()
     analyser = audioCtx.createAnalyser()
-if (navigator.mediaDevices) {
-    navigator.mediaDevices.getDisplayMedia ({audio: true, video: true})
-    .then(function(stream) {
-        initiateNode(stream)
-    })
-    .catch(function(err) {
-        console.log('getUserMedia error' + err)
-    })
-    // analyser.connect(audioCtx.destination)
-}
+    if (navigator.mediaDevices) {
+        navigator.mediaDevices.getDisplayMedia({ audio: true, video: true })
+            .then(function (stream) {
+                initiateNode(stream)
+            })
+            .catch(function (err) {
+                console.log('getUserMedia error' + err)
+            })
+        // analyser.connect(audioCtx.destination)
+    }
 }
 
 
@@ -56,13 +54,13 @@ canvas.setAttribute('height', document.querySelector('body').clientHeight)
 
 let colorOffset = 360
 
-let visualise = function(detail = 256) {
+let visualise = function (detail = 256) {
     // detail = 32768
     analyser.fftSize = detail // determines detail
     var bufferLength = analyser.fftSize
     var dataArray = new Uint8Array(bufferLength)
     canvasCtx.clearRect(0, 0, canvas.width, canvas.height)
-    let draw = function(colorOffset) {
+    let draw = function (colorOffset) {
         drawVisual = requestAnimationFrame(draw)
         analyser.getByteTimeDomainData(dataArray)
         let max = [...dataArray].sort()[dataArray.length - 1]
@@ -73,7 +71,7 @@ let visualise = function(detail = 256) {
         // let hi = dataArray.sort()[dataArray.length - 1]
         // max = 0
         // canvasCtx.strokeStyle = 'rgb(' + max + ',0,0)'
-        let hue = ~~ (360 * max / 255)
+        let hue = ~~(360 * max / 255)
         // console.log(hue)
         // canvasCtx.strokeStyle = 'hsl(' + hue + '100%, 50%)'
         canvasCtx.strokeStyle = 'hsl(' + Math.abs(colorOffset - hue) + ', 100%, 50%)'
@@ -84,7 +82,7 @@ let visualise = function(detail = 256) {
         let sliceWidth = canvas.width * 1.0 / bufferLength
         let x = 0
         // console.log(max)
-        
+
         for (let i = 0; i < bufferLength; i++) {
             let v = dataArray[i] / 128.0
             // v = dataArray.sort()[dataArray.length - 1]  / 128.0
@@ -110,26 +108,25 @@ let parameterChange = false
 let clientX
 let clientY
 
-canvas.addEventListener('mousedown', function(e) {
+canvas.addEventListener('mousedown', function (e) {
     console.log('mousedown started')
     parameterChange = true
     clientX = e.offsetX
     clientY = e.offsetY
 })
 
-canvas.addEventListener('mouseup', function() {
+canvas.addEventListener('mouseup', function () {
     console.log('mousedown finished')
     if (parameterChange) {
         parameterChange = false
     }
 })
 
-canvas.addEventListener('mousemove', function(e) {
+canvas.addEventListener('mousemove', function (e) {
     if (parameterChange) {
         let old = colorOffset
-        // console.log('off: ' + colorOffset)
         y = e.offsetY
-        colorOffset += (10 * (y - clientY))
+        colorOffset += (0.1 * (y - clientY))
         // console.log('new: ' +  colorOffset)
         log.innerText = 'off: ' + old + '&nbsp;' + 'new: ' + colorOffset
     }
